9af937e1789b9cd3fa1e9c8287cfbd7aaf3213b4
Abdullah Hali Saputra <abdullahhali060@gmail.com>
2023-09-11 15:40:48 +0700

    change MyBackgroundThread using kotlin coroutine

--- MyBackgroundThread/app/src/main/java/com/dicoding/mybackgroundthread/MainActivity.kt
+++ MyBackgroundThread/app/src/main/java/com/dicoding/mybackgroundthread/MainActivity.kt
@@ -2,10 +2,12 @@ package com.dicoding.mybackgroundthread
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
+import androidx.lifecycle.lifecycleScope
 import com.dicoding.mybackgroundthread.databinding.ActivityMainBinding
-import java.util.concurrent.Executors
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -14,26 +16,19 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val executor = Executors.newSingleThreadExecutor()
-        val handler = Handler(Looper.getMainLooper())
-
-        binding. apply {
-            btnStart.setOnClickListener{
-                executor.execute{
-                    try {
-                        for(i in 0..10) {
-                            Thread.sleep(500)
-                            val percentage = i * 10
-                            handler.post {
-                                if (percentage == 100) {
-                                    tvStatus.setText(R.string.task_completed)
-                                }else {
-                                    tvStatus.text = String.format(getString(R.string.compressing), percentage)
-                                }
+        binding.apply {
+            btnStart.setOnClickListener {
+                lifecycleScope.launch(Dispatchers.Default) {
+                    for (i in 0..10) {
+                        delay(500)
+                        val percentage = i * 10
+                        withContext(Dispatchers.Main) {
+                            if (percentage == 100) {
+                                tvStatus.setText(R.string.task_completed)
+                            } else {
+                                tvStatus.text = String.format(getString(R.string.compressing), percentage)
                             }
                         }
-                    }catch (e: InterruptedException) {
-                        e.printStackTrace()
                     }
                 }
             }
